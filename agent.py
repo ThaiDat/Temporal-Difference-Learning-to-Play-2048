65b0a4f37948c2903665897dcfb4b2b64e52c934
ThaiDat <tandatcr2000pro@yahoo.com>
2021-11-06 08:30:24 +0700

    change approach to td afterstate

--- agent.py
+++ agent.py
@@ -1,55 +1,96 @@
 from globalconfig import gconfig
 import torch
 from torch import nn
+from torch.nn import functional as F
+from torch.nn import utils
 
 
-class DQN2048Agent(nn.Module):
-    '''Pytorch network to learn to play 2048'''
-    def __init__(self, epsilon=gconfig['INITIAL_EPSILON']):
+class Network(nn.Module):
+    '''Pytorch network to learn values function'''
+    def __init__(self):
         '''
         epsilon: initial value of epsilon greedy policy
         '''
-        super().__init__()
-        self.epsilon = epsilon
-        self.n_actions = 4
-        self.qnetwork = nn.Sequential(
+        super(Agent, self).__init__()
+        self.hoz_conv = nn.Sequential(
+            nn.Conv2d(16, 64, (2,1)), # 64x3x4
+            nn.ReLU(),
+            nn.Conv2d(64, 128, (2, 1)), # 128x2x4
+            nn.ReLU(),
+            nn.Conv2d(128, 256, (2, 1)), # 256x1x4
+            nn.ReLU(),
+            nn.Flatten(), # 1024
+        )
+        self.ver_conv = nn.Sequential(
+            nn.Conv2d(16, 64, (1, 2)), # 64x4x3
+            nn.ReLU(),
+            nn.Conv2d(64, 128, (1, 2)), # 128x4x2
+            nn.ReLU(),
+            nn.Conv2d(128, 256, (1, 2)), # 256x4x1
+            nn.ReLU(),
+            nn.Flatten(), # 1024
+        )
+        self.box_conv = nn.Sequential(
             nn.Conv2d(16, 64, 2), # 64x3x3
             nn.ReLU(),
             nn.Conv2d(64, 128, 2), # 128x2x2
             nn.ReLU(),
-            nn.Conv2d(128, 512, 2), # 512x1x1
+            nn.Conv2d(128, 256, 2), # 256x1x2
             nn.ReLU(),
-            nn.Flatten(), # 512
-            nn.Linear(512, 512), # 512
+            nn.Flatten(), # 256
+        )
+        self.v = nn.Sequential(
+            nn.Linear(1024+1024+256, 1024), # 1024
             nn.ReLU(),
-            nn.Linear(512, 256), # 256
-            nn.ReLU(),
-            nn.Linear(256, 128), # 128
-            nn.ReLU(),
-            nn.Linear(128, 64), # 64
-            nn.ReLU(),
-            nn.Linear(64, self.n_actions) # 4
+            nn.Linear(1)
         )
 
     def forward(self, states):
         '''
         Forward the states tensor to receive tensor of qvalues
         states: tensor of states batch x 16x4x4
-        return tensor of qvalues batch x n_actions
+        return logit and state values
         '''
-        # Use your network to compute qvalues for given state
-        qvalues = self.qnetwork(states) # <YOUR CODE>
-        return qvalues
+        hoz = self.hoz_conv(states) 
+        ver = self.ver_conv(states)
+        box = self.box_conv(states)
+        fs = torch.cat((hoz, ver, box), -1)
+        v = self.v(fs)
+        return v
 
-    def sample_actions(self, qvalues):
+
+class Model:
+    '''
+    Model of value function. Wrapper to train the network
+    '''
+    def __init__(self, make_network=Network, optim=gconfig['OPTIMIZER'], lr=gconfig['LEARNING_RATE']):
         '''
-        Pick actions given qvalues. Uses epsilon-greedy exploration strategy.
-        qvalues: Tensor of qvalues batch x n_actions
-        return tensor of actions batch
+        make_network: function or class to init network
+        optim: Name of optimizer
+        lr: Learning rate
         '''
-        max_indices = torch.argmax(qvalues, dim=-1)
-        minor_prob = self.epsilon / self.n_actions
-        major_prob = 1 - self.epsilon + minor_prob
-        probs = torch.full(qvalues.size(), minor_prob)
-        probs[torch.arange(qvalues.size(0)), max_indices] = major_prob
-        return torch.multinomial(probs, 1)
+        self.network = make_network()
+        self.optim = getattr(torch.optim, optim)(self.network.parameters(), lr=lr)
+
+    def predict(self, states):
+        '''
+        Predict values of given states
+        states: tensor of encoded states. batch x16x4x4
+        return state values batch
+        '''
+        with torch.no_grad():
+            return self.network(states)
+    
+    def fit(self, states, values, max_grad_norm=gconfig['MAX_GRADIENT_NORM']):
+        '''
+        Train the network with given datas
+        states: tensor of encoded states. batch x16x4x4
+        values: tensor of state values. batch
+        return loss, gradient values
+        '''
+        pr = self.network(states)
+        self.optim.zero_grad()
+        loss = F.smooth_l1_loss(pr, values)
+        grad_norm = utils.clip_grad_norm_(self.network.parameters(), max_grad_norm)
+        self.optim.step()
+        return loss.cpu().item(), grad_norm.cpu().item()
--- globalconfig.py
+++ globalconfig.py
@@ -20,26 +20,19 @@ gconfig['ACTION_MAP'] = {0:'LEFT', 1:'UP', 2:'RIGHT', 3:'DOWN'}
 
 # Sleep time after sending action to the game
 # This is for avoiding error when you paly too fast
-gconfig['ACTION_SLEEP'] = 0.02
+gconfig['ACTION_SLEEP'] = 0.1
 
 # Scale the reward for the q-value approximator not to learn too big values
 gconfig['REWARD_SCALE'] = 1/2048
 
-# Size of experiences replay
-gconfig['EXPERIENCE_BUFFER'] = 4000
-
-# Minimum of experiences to sample from
-# If buffer has less experience than this number, sample nothing
-gconfig['MIN_EXPERIENCE'] = 2000
-
-# Size of batch sample from experiences buffer to train
-gconfig['BATCH'] = 32
+# Number of environments / batch size in A3C
+gconfig['BATCH'] = 64
 
 # Gradient descent optimizer. Must be exact the same with torch optim class name
 gconfig['OPTIMIZER'] = 'Adam'
 
 # Learning rate of model
-gconfig['LEARNING_RATE'] = 5e-5
+gconfig['LEARNING_RATE'] = 1e-4
 
 # Clip the gradient norm to this number.
 gconfig['MAX_GRADIENT_NORM'] = 50
@@ -51,32 +44,22 @@ gconfig['CHANEL_ENCODED'] = 16
 gconfig['DEVICE'] = 'cuda'
 
 # Discounted factor gamma in Bellman (optimality) equation. How reward in later states affect the current one
-gconfig['DISCOUNTED'] = 0.99
-
-# Probability for choosing random actions while training
-gconfig['INITIAL_EPSILON'] = 0.5
-
-# Minimum probability for choosing random actions while training. This is for keeping exploring to find better policy
-gconfig['MIN_EPSILON'] = 0.05
-
-# The epsilon decrement every steps until reach minimum value
-# Epsilon need decay overtime (less exploration, more exploitation). 
-gconfig['EPSILON_LINEAR_DECAY'] = (0.5 - 0.05) / 8000
+gconfig['DISCOUNTED'] = 1
 
 # We will just monitor some number of last steps. For example -500 means that only last 500 steps will display on the screen
 gconfig['MONITOR_RANGE'] = -500
 
 # Total number of training steps
-gconfig['TRAIN_STEPS'] = 10000
-
-# After some steps, we will update target network parameter using learning network/agent
-gconfig['UPDATE_STEPS'] = 400
+gconfig['TRAIN_STEPS'] = 40000
 
 # We will updates some information like gradient and loss after some steps to help monitor the training process
-gconfig['MONITOR_STEPS'] = 10
+gconfig['MONITOR_STEPS'] = 20
 
 # Save the models to file every n steps
 gconfig['BACKUP_STEPS'] = 1000
+
+# Evaluate the agent every n steps
+gconfig['EVALUATE_STEPS'] = 500
 
 # The location to save model
 gconfig['BACKUP_LOCATION'] = path.join('bin', 'model.rl')
